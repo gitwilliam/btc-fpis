56b9af3511a3cdf124d6a4c23c96a2dd3412ac21
Bill Harding <william.paul.harding@gmail.com>
2018-08-28 20:50:07 -0500

    Initialize Project

---
+++ src/main/scala/com.billtheconqueror.exercises/ChapterTwo.scala
@@ -0,0 +1,7 @@
+package com.billtheconqueror.exercises
+
+object ChapterTwo {
+  def example(): Boolean = {
+    true
+  }
+}
---
+++ src/test/scala/com/billtheconqueror/exercises/ChapterTwoTest.scala
@@ -0,0 +1,9 @@
+package com.billtheconqueror.exercises
+import org.scalatest.FunSuite
+
+class ChapterTwoTest extends FunSuite {
+  test("Example") {
+    assert(ChapterTwo.example());
+  }
+
+}
